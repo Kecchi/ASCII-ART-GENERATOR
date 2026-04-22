6fb81076a3cf92c506bceb137bec205f82b2b88e
Caitie <63966525+Kecchi@users.noreply.github.com>
2026-04-22 17:55:35 +1000

    Update ASCII ART GENERATOR.py

--- ASCII ART GENERATOR.py
+++ ASCII ART GENERATOR.py
@@ -1,5 +1,6 @@
 # Make a shape artwork with a repeating word, the * get replaced
 import sys
+import random
 bitmap = """
 ....................................................................
    **************   *  *** **  *      ******************************
@@ -23,40 +24,120 @@ bitmap = """
                     **     *                    *
 ...................................................................."""
 
-#smile """
-#...................................................................
-#                  ****                 ****                        
-#                *******               ******                       
-#                  ****                 ****                        
-#                                                                   
-#         **                                    **                  
-#         ****                                ****                  
-#           *****                           *****                   
-#                ******               ******                        
-#                      ****************                             
-#..................................................................."""
+smile = """
+...................................................................
+                  ****                 ****                                                
+                 ******               ******                                              
+                  ****                 ****                                                
+                                                                                                                                                      
+         **                                    **                                    
+         ****                                ****                                    
+           *****                           *****                                      
+                ******               ******                                                
+                      ****************                                                          
+..................................................................."""
 
-#
+peace = """
+...................................................................
+                                                                          
+                    *******                  
+                   **     **                  
+                  **      **                  
+                  **      **      
+                  **      **                  
+                  **      **                  
+                  **      **                  
+                  **      **                  
+                 ***      ***                  
+                 ***      ***                 
+           *********       ***                    
+          ***    **        ******               
+         ***      *        ***    ***             
+      ****        *        *       **           
+   ******         *        *       *****             
+  **    *         *        *       **  ***
+***    **        **       **       **    **
+**     **                          **     ***
+*      **                           **      **
+**     **                                    **
+****                                         **
+ ****                                       ***
+   ****                                    ***
+     ****                                ***
+       *****                           ****
+          *****                       ***                 
+            ***                      ***                 
+             **                      **                 
+             **                     **                 
+             **                     **                 
+             *************************                 
+                                                                          
+..................................................................."""
+
+
+sass = ["EXCUSE ME??", "Thank god ur not a moron...oh wait.", "This shit can't be serious", "Bro...not cool.", "DUDE COME ON!", "If this was real I would be so mad"]
+question = ("Would you like to try 'map', 'smile' or 'peace' ?")
+choices = ["map", "smile", "peace"]
+
+
+#8================================================================================
+
+
 while True:
 
     print ('Create artwork with a word, shitpost potential ahead.')
-#    print ("Would you like to try 'map', 'smile' or 'alpha' ?")
+    print (question)
+    
+    answer = input('>').lower()
+    if answer not in choices:
+        print (random.choice(sass))
 
-    print ('Enter the message to display with the bitmap NOW!')
-    message = input('>')
-    if message == '':
-        sys.exit()
+    if answer == "map":
+        print ('Enter the message to display with the bitmap NOW!')
+        message = input('>')
+        if message == '':
+            sys.exit()
 
-    for line in bitmap.splitlines():
-        for i, bit in enumerate(line):
-            if bit == ' ':
-                print (' ', end='')
-            else:
-                print(message[i % len(message)], end='')
-        print()
+        for line in bitmap.splitlines():
+            for i, bit in enumerate(line):
+                if bit == ' ':
+                    print (' ', end='')
+                else:
+                    print(message[i % len(message)], end='')
+            print()
+
+    if answer == "smile":
+        print ('Enter the message to display with the smile NOW!')
+        message = input('>')
+        if message == '':
+            sys.exit()
+
+        for line in smile.splitlines():
+            for i, bit in enumerate(line):
+                if bit == ' ':
+                    print (' ', end='')
+                else:
+                    print(message[i % len(message)], end='')
+            print()
+
+    if answer == "peace":
+        print ('Enter the message to display with the peace NOW!')
+        message = input('>')
+        if message == '':
+            sys.exit()
+
+        for line in peace.splitlines():
+            for i, bit in enumerate(line):
+                if bit == ' ':
+                    print (' ', end='')
+                else:
+                    print(message[i % len(message)], end='')
+            print()
+
+#8================================================================================
 
     while True:
-        choice = input("Play again? (y/n): ").lower()
+        choice = input ("Play again? (y/n): ").lower()
 
         if choice in ["y", "n"]:
             break
